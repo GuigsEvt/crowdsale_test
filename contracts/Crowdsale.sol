0710cb20cd460e32a8efb2657eb6a72b54c5d073
guigs <evrat.guillaume@gmail.com>
2018-10-17 14:41:17 +0200

    add crowdsale files

---
+++ contracts/Crowdsale.sol
@@ -0,0 +1,69 @@
+/* pragma solidity ^0.4.14;
+
+import './TESTToken.sol';
+import './SafeMath.sol';
+
+contract Crowdsale {
+  using SafeMath for uint256;
+
+  // The token being sold
+  TESTToken public token;
+
+  // start and end timestamps where investments are allowed (both inclusive)
+  uint256 public startTime;
+  uint256 public endTime;
+
+  // address where funds are collected
+  address public wallet;
+
+  // how many token units a buyer gets per wei
+  uint256 public rate;
+
+  // amount of raised money in wei
+  uint256 public weiRaised;
+
+  event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
+
+
+  function Crowdsale(uint256 _startTime, uint256 _endTime, uint256 _rate, address _wallet) {
+    // To be defined
+  }
+
+
+  // creates the token to be sold.
+  // override this method to have crowdsale of a specific mintable token.
+  function createTokenContract() internal returns (TESTToken) {
+    // To be defined
+  }
+
+
+  // fallback function can be used to buy tokens
+  function () payable {
+    buyTokens(msg.sender);
+  }
+
+  // low level token purchase function
+  function buyTokens(address beneficiary) public payable {
+    // To be defined
+  }
+
+  // send ether to the fund collection wallet
+  // override to create custom fund forwarding mechanisms
+  function forwardFunds() internal {
+    wallet.transfer(msg.value);
+  }
+
+  // @return true if the transaction can buy tokens
+  function validPurchase() internal constant returns (bool) {
+    bool withinPeriod = block.number >= startTime && block.number <= endTime;
+    bool nonZeroPurchase = msg.value != 0;
+    return withinPeriod && nonZeroPurchase;
+  }
+
+  // @return true if crowdsale event has ended
+  function hasEnded() public constant returns (bool) {
+    return block.number > endTime;
+  }
+
+
+} */
---
+++ contracts/FinalizableCrowdsale.sol
@@ -0,0 +1,27 @@
+/* pragma solidity ^0.4.14;
+
+import './SafeMath.sol';
+import './Ownable.sol';
+import './Crowdsale.sol';
+
+contract FinalizableCrowdsale is Crowdsale, Ownable {
+  using SafeMath for uint256;
+
+  bool public isFinalized = false;
+
+  event Finalized();
+
+  function finalize() onlyOwner public {
+    require(!isFinalized);
+    require(hasEnded());
+
+    finalization();
+    Finalized();
+
+    isFinalized = true;
+  }
+
+  function finalization() internal {
+  }
+
+} */
